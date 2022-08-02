7974b9c34c4b82c5dd02eabd8a9670b55ce248bc
amateurCrypt <muhammad.qasim1@live.com>
2022-08-02 14:49:47 -0400

    add JS file

---
+++ script.js
@@ -0,0 +1,10 @@
+const inpFile = document.getElementById("inpFile")
+const previewContainer = document.getElementById("imagePreview");
+const previewImage = previewContainer.querySelector(".image-preview__image")
+const previewDefaultText = previewContainer.querySelector(".image-preview__default-text")
+
+inpFile.addEventListener("change", function(){
+    const file = this.files[0];
+
+    console.log(file)
+})
